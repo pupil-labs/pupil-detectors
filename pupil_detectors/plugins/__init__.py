2be2fa0fd362fedcb8aa9efa3c42198999e86dd1
Roman Roibu <romanroibu@gmail.com>
2019-09-20 19:11:14 +0200

    Expose pupil_detectors.plugins module symbols

---
+++ pupil_detectors/plugins/__init__.py
@@ -0,0 +1,5 @@
+from .detector_base_plugin import PupilDetectorPlugin
+from .detector_dummy_plugin import DetectorDummyPlugin
+from .detector_2d_plugin import Detector2DPlugin
+from .detector_3d_plugin import Detector3DPlugin
+
