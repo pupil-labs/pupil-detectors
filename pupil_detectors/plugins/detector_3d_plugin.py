696a81b869fb6ac3dbb7fc5c23d2fb3e34d65d97
Roman Roibu <romanroibu@gmail.com>
2019-09-23 12:26:30 +0200

    Add helper methods to manage debug window in Detector3DPlugin

--- pupil_detectors/plugins/detector_3d_plugin.py
+++ pupil_detectors/plugins/detector_3d_plugin.py
@@ -32,7 +32,7 @@ class Detector3DPlugin(PupilDetectorPlugin):
             user_roi,
             visualize,
             pause_video,
-            is_debugging_enabled=self.debugVisualizer3D != None
+            is_debugging_enabled=self.is_debug_window_open
         )
 
     @property
@@ -61,7 +61,7 @@ class Detector3DPlugin(PupilDetectorPlugin):
         info_3d = ui.Info_Text("Open the debug window to see a visualization of the 3D pupil detection." )
         self.menu.append(info_3d)
         self.menu.append(ui.Button('Reset 3D model', self.reset_model))
-        self.menu.append(ui.Button('Open debug window',self.toggle_window))
+        self.menu.append(ui.Button('Open debug window',self.debug_window_toggle))
         model_sensitivity_slider = ui.Slider(
             'model_sensitivity',
             self.detector_properties_3d,
@@ -100,18 +100,33 @@ class Detector3DPlugin(PupilDetectorPlugin):
     def pretty_class_name(self):
         return 'Pupil Detector 3D'
 
+    @property
+    def is_debug_window_open(self) -> bool:
+        return self.debugVisualizer3D.window is not None
+
+    def debug_window_toggle(self):
+        if not self.is_debug_window_open:
+            self.debug_window_open()
+        else:
+            self.debug_window_close()
+
+    def debug_window_open(self):
+        if not self.is_debug_window_open:
+            self.debugVisualizer3D.open_window()
+
+    def debug_window_close(self):
+        if self.is_debug_window_open:
+            self.debugVisualizer3D.close_window()
+
+    def debug_window_update(self):
+        if self.is_debug_window_open:
+            self.debugVisualizer3D.update_window(self.g_pool, self.detector_3d.debug_result)
+
     def reset_model(self):
          self.detector_3d.reset_model()
 
-    def toggle_window(self):
-        if not self.debugVisualizer3D.window:
-            self.debugVisualizer3D.open_window()
-        else:
-            self.debugVisualizer3D.close_window()
-
     def visualize(self):
-        if self.debugVisualizer3D.window:
-            self.debugVisualizer3D.update_window(self.g_pool, self.detector_3d.debug_result)
+        self.debug_window_update()
 
     def cleanup(self):
-        self.debugVisualizer3D.close_window() # if we change detectors, be sure debug window is also closed
+        self.debug_window_close() # if we change detectors, be sure debug window is also closed
