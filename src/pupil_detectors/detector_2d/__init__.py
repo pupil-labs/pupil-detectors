8a00468e5c93b6d2a32a9c67f8e57c5876d11825
Patrick Faion <p.faion@web.de>
2019-12-02 13:13:11 +0100

    Add missing license headers

--- src/pupil_detectors/detector_2d/__init__.py
+++ src/pupil_detectors/detector_2d/__init__.py
@@ -1,2 +1,13 @@
+"""
+(*)~---------------------------------------------------------------------------
+Pupil - eye tracking platform
+Copyright (C) 2012-2019 Pupil Labs
+
+Distributed under the terms of the GNU
+Lesser General Public License (LGPL v3.0).
+See COPYING and COPYING.LESSER for license details.
+---------------------------------------------------------------------------~(*)
+"""
+
 # rename from python side, since Detector2D is already defined in cython from c++
 from .detector_2d import Detector2DCore as Detector2D
