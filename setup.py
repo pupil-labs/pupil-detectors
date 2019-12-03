96962c58464181cb312a2e7a357d611d2d37b3e0
Patrick Faion <pfa@pupil-labs.com>
2019-12-03 11:34:03 +0100

    Bump version: 0.3.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -265,6 +265,6 @@ if __name__ == "__main__":
             package_data={package: package_data},
             package_dir={"": package_dir},
             url="https://github.com/pupil-labs/pupil-detectors",
-            version="0.3.0",
+            version="1.0.0",
             zip_save=False,
         )
