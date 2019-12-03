2c7feb758f564874dd267203a7cf4d7c32603fc3
Patrick Faion <pfa@pupil-labs.com>
2019-12-03 11:52:47 +0100

    Bump version: 1.0.0 → 1.0.1

--- src/pupil_detectors/__init__.py
+++ src/pupil_detectors/__init__.py
@@ -9,7 +9,7 @@ See COPYING and COPYING.LESSER for license details.
 ---------------------------------------------------------------------------~(*)
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 import platform
 
