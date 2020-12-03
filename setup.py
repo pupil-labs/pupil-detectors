7969b6bdacfbf9c4e766d9f71043204c59b7ed2b
Pablo Prietz <pablo@prietz.org>
2020-12-03 17:11:51 +0100

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -244,6 +244,6 @@ if __name__ == "__main__":
             package_data={package: package_data},
             package_dir={"": package_dir},
             url="https://github.com/pupil-labs/pupil-detectors",
-            version="2.0.0",
+            version="2.0.1",
             zip_save=False,
         )
